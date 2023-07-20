9592c8f471a6e08ddd043b7e5aa850fa9bee15b9
debiabril <debora.badosa@gmail.com>
2023-07-20 17:32:35 -0300

    scrollear narvbar REVISAR SUPERPOSICION

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const botonDeContacto = document.getElementById('botonDeContacto');
 botonDeContacto.addEventListener('click', () => {
     window.scrollTo(0, 2000)
 })
+
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});

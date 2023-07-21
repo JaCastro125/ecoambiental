e8922992fd3490622328c7383d8c656b47bc4bbd
MarceloZych <74663103+MarceloZych@users.noreply.github.com>
2023-07-21 12:23:17 -0300

    agregamos el circulo

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const botonDeContacto = document.getElementById('botonDeContacto');
 botonDeContacto.addEventListener('click', () => {
     window.scrollTo(0, 2000)
 })
+
+const iconElements = document.querySelectorAll('.icon');
+
+iconElements.forEach(icon => {
+  icon.addEventListener('click', () => {
+    const textId = icon.id.replace('icon', 'text');
+    const text = document.getElementById(textId);
+    text.style.display = text.style.display === 'none' ? 'block' : 'none';
+  });
+});
